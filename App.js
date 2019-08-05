d70bcd1163ba97dce3e678a1239a44f4d920ab0a
Kevin Damian Ta <kevindamianta@gmail.com>
2019-08-04 17:10:55 -0700

    Initial Components for Trelloyes
    Setting up initial component structure for Trelloyes excercise

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Card from "./Card.js"
+
+class App extends React.Component {
+    constructor() {
+
+    }
+
+    render() {
+        return(
+            
+        )
+    }
+}
+
+export default App
---
+++ Card.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+class Card extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div className="card">
+                
+            </div>
+        )
+
+    }
+}
+
+export default Card
